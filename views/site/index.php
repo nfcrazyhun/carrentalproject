faa2f87fe13ea5d24e175f2f400b3a9783e588e9
CrazyOne <crazy@one.da>
2019-08-12 13:37:11 +0200

    Fix img path

--- views/site/index.php
+++ views/site/index.php
@@ -11,7 +11,7 @@ $this->title = 'My Yii Application';
 
         <p class="lead">Hello, and welcome to the site!</p>
 
-        <p><img src="best-car-rentals-in.jpg" alt="Smiley face"></p>
+        <p><img src="../best-car-rentals.jpg" alt="Smiley face"></p>
 
 <!--        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>-->
     </div>
